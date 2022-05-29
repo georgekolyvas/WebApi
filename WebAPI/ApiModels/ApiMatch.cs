75fec1a02413a0ecce1ae4deb0049481d75aa0d3
George Kolyvas <kolyvasgiorgos@hotmail.com>
2022-05-29 17:54:16 +0300

    Updated .gitignore + Updated MatchController

--- WebAPI/ApiModels/ApiMatch.cs
+++ WebAPI/ApiModels/ApiMatch.cs
@@ -7,15 +7,15 @@ namespace WebAPI.ApiModels
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        
         public DateTime MatchDate { get; set; }
-        // We will get time from the MatchDate property
-        //public TimeSpan MatchTime { get; set; }
+        
         [Range(0, 23)]
         public int Hour{ get; set; }
 
         [Range(0, 59)]
         public int Minutes { get; set; }
-        //
+        
         public string TeamA { get; set; }
         public string TeamB { get; set; }
         public int Sport { get; set; }
